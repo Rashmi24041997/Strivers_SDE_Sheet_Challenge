ad9fa483eacf24fdc8f863e4b7822133bb648a02
Rashmi Gupta <rashmimittal2497@gmail.com>
2023-06-23 02:01:14 +0530

    Solution for set zero matrix

---
+++ Strivers_SDE_Sheet_Challenge/Easy/Arrays/Set Matrix Zeroes.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strivers_SDE_Sheet_Challenge.Easy.Arrays
+{
+    public class Set_Matrix_Zeroes
+    {
+        public static int[][] Sol(int[][] a)
+        {
+            int r = a.Length;
+            int c = a[0].Length;
+            if (r <= 0 || (r == 1 && c == 1)) return a;
+            List<int> ro = new();
+            List<int> col = new();
+            for (int i = 0; i < r; i++)
+                for (int j = 0; j < a[i].Length; j++)
+                    if (a[i][j] == 0)
+                    {
+                        if (!ro.Contains(i))
+                            ro.Add(i);
+                        if (!col.Contains(j))
+                            col.Add(j);
+                    }
+            if (!(col.Count == c) && ro.Count > 0)
+                    foreach (int i in ro)
+                        for (int j = 0; j < a[i].Length; j++)
+                            a[i][j] = 0;
+            if (((ro.Count == r ) && (col.Count == c)) || col.Count > 0)
+                foreach (int i in col)
+                    for (int j = 0; j < r; j++)
+                        a[j][i] = 0;
+            return a;
+        }
+    }
+}
---
+++ Strivers_SDE_Sheet_Challenge/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Strivers_SDE_Sheet_Challenge.Easy.Arrays;
+
+Console.WriteLine("Hello, World!");
+int[] ints = new int[4] {1,2,3,4};
+int[][] a = new int[][] { new int[]{ 0, 1, 2, 3 }, new int[] { 3, 0, 5, 2 }, new int[]{ 1, 3, 0, 5 }, new int[] { 1, 3, 9, 0 } };
+
+ System.Console.WriteLine(Set_Matrix_Zeroes.Sol(a));
